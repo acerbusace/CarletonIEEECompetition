1de9e475709bc70f1c2ff803e3515c108a213cb8
acerbusace <acerbus.ace@gmail.com>
2015-10-17 20:16:05 -0400

    Recent

---
+++ CarletonCompetition/src/imageProcessing/mainFile.java
@@ -0,0 +1,21 @@
+package imageProcessing;
+
+import java.awt.Image;
+
+public class mainFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String imagePATH = "Directory/Samples/t";
+		String backgroundPath = "Directory/Background/";
+		String ext = ".bmp";
+		
+		int imageNum = 1;
+		Image hello;
+		
+		while(true){
+			
+		}
+	}
+
+}
